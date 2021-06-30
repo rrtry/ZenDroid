556424c3c2d9bf3a028ff13c07957083086ad2c8
rrtry <fedormocalov36@gmail.com>
2021-06-30 13:14:20 +0300

    Removed redunant classes

--- app/src/main/java/com/example/volumeprofiler/fragments/ConversationsRestrictionsDialog.kt
+++
@@ -1,5 +0,0 @@
-package com.example.volumeprofiler.fragments
-
-class ConversationsRestrictionsDialog {
-
-}
--- app/src/main/java/com/example/volumeprofiler/fragments/MessagesRestrictionsDialog.kt
+++
@@ -1,4 +0,0 @@
-package com.example.volumeprofiler.fragments
-
-class MessagesRestrictionsDialog {
-}
